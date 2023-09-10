631d3fc2abe06dcd73c86e2a253043c1da0ca877
gokulhans <gokulhansv@gmail.com>
2023-09-10 14:22:38 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Users from "./pages/Users/Users";
 import Notes from "./pages/Notes/Notes";
+import AddUser from "./pages/AddUser/AddUser";
 
 function App() {
   return (
@@ -13,8 +14,9 @@ function App() {
         <div className="ml-64 bg-gray-50 dark:bg-gray-900 h-screen dark:text-white">
           <Routes>
             <Route exact path="/" element={<Home />} />
-            <Route exact path="/users" element={<Users />} />
-            <Route exact path="/notes" element={<Notes />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/notes" element={<Notes />} />
+            <Route path="/adduser" element={<AddUser />} />
           </Routes>
         </div>
       </Router>
--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -1,20 +1,19 @@
-import { faMagnifyingGlass, faPlus } from "@fortawesome/free-solid-svg-icons";
+import {
+  faMagnifyingGlass,
+  faPenToSquare,
+  faTrash,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Users = () => {
   return (
     <>
       <section className="bg-gray-50 dark:bg-gray-900 flex items-start pt-5">
         <div className="max-w-screen-xl px-4 mx-auto lg:px-12 w-full">
-          <div className="relative bg-white dark:bg-gray-800">
+          <div className="relative bg-white shadow-md dark:bg-gray-800 rounded-t">
             <div className="flex flex-col items-center justify-between p-4 space-y-3 md:flex-row md:space-y-0 md:space-x-4">
-              <h2
-                type="button"
-                className="flex items-center justify-center px-4 py-2 text-sm font-medium text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
-              >
-                Add User
-              </h2>
               <div className="w-full md:w-1/2">
                 <form className="flex items-center">
                   <label htmlFor="simple-search" className="sr-only">
@@ -35,169 +34,67 @@ const Users = () => {
                 </form>
               </div>
               <div className="flex flex-col items-stretch justify-end flex-shrink-0 w-full space-y-2 md:w-auto md:flex-row md:space-y-0 md:items-center md:space-x-3">
-                <button
+                <Link
+                  to={"/adduser"}
                   type="button"
                   className="flex items-center justify-center px-4 py-2 text-sm font-medium text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
                 >
                   Add User
-                </button>
+                </Link>
               </div>
             </div>
           </div>
         </div>
       </section>
 
-      <div class="overflow-x-auto max-w-screen-xl px-4 mx-auto lg:px-12 w-full ">
-        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-          <thead class="text-xs text-gray-400 uppercase bg-white shadow-md dark:bg-gray-800">
+      <div class="overflow-x-auto max-w-screen-xl px-4 mx-auto lg:px-12 w-full rounded-b">
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400 rounded-b">
+          <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
               <th scope="col" class="px-4 py-3">
-                Product name
+                Name
               </th>
               <th scope="col" class="px-4 py-3">
-                Category
+                Number
               </th>
               <th scope="col" class="px-4 py-3">
-                Brand
+                Date of Joining
+              </th>{" "}
+              <th scope="col" class="px-4 py-3">
+                Height
+              </th>{" "}
+              <th scope="col" class="px-4 py-3">
+                Weight
               </th>
               <th scope="col" class="px-4 py-3">
-                Description
+                Photo
               </th>
               <th scope="col" class="px-4 py-3">
-                Price
-              </th>
+                Payment Status
+              </th>{" "}
               <th scope="col" class="px-4 py-3">
-                <span class="sr-only">Actions</span>
+                Actions
               </th>
             </tr>
           </thead>
-          <tbody>
-            <tr class="border-b dark:border-gray-700">
-              <th
-                scope="row"
-                class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                Apple iMac 27&#34;
-              </th>
-              <td class="px-4 py-3">PC</td>
-              <td class="px-4 py-3">Apple</td>
-              <td class="px-4 py-3">300</td>
-              <td class="px-4 py-3">$2999</td>
-              <td class="px-4 py-3 flex items-center justify-end">
-                <button
-                  id="apple-imac-27-dropdown-button"
-                  data-dropdown-toggle="apple-imac-27-dropdown"
-                  class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100"
-                  type="button"
-                >
-                  <svg
-                    class="w-5 h-5"
-                    aria-hidden="true"
-                    fill="currentColor"
-                    viewbox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
-                  </svg>
-                </button>
-                <div
-                  id="apple-imac-27-dropdown"
-                  class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600"
-                >
-                  <ul
-                    class="py-1 text-sm text-gray-700 dark:text-gray-200"
-                    aria-labelledby="apple-imac-27-dropdown-button"
-                  >
-                    <li>
-                      <a
-                        href="#"
-                        class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                      >
-                        Show
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href="#"
-                        class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                      >
-                        Edit
-                      </a>
-                    </li>
-                  </ul>
-                  <div class="py-1">
-                    <a
-                      href="#"
-                      class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                    >
-                      Delete
-                    </a>
-                  </div>
-                </div>
-              </td>
-            </tr>
-            <tr class="border-b dark:border-gray-700">
-              <th
-                scope="row"
-                class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-              >
-                Apple iMac 20&#34;
-              </th>
-              <td class="px-4 py-3">PC</td>
-              <td class="px-4 py-3">Apple</td>
-              <td class="px-4 py-3">200</td>
-              <td class="px-4 py-3">$1499</td>
-              <td class="px-4 py-3 flex items-center justify-end">
-                <button
-                  id="apple-imac-20-dropdown-button"
-                  data-dropdown-toggle="apple-imac-20-dropdown"
-                  class="inline-flex items-center p-0.5 text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100"
-                  type="button"
-                >
-                  <svg
-                    class="w-5 h-5"
-                    aria-hidden="true"
-                    fill="currentColor"
-                    viewbox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
-                  </svg>
-                </button>
-                <div
-                  id="apple-imac-20-dropdown"
-                  class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600"
-                >
-                  <ul
-                    class="py-1 text-sm text-gray-700 dark:text-gray-200"
-                    aria-labelledby="apple-imac-20-dropdown-button"
-                  >
-                    <li>
-                      <a
-                        href="#"
-                        class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                      >
-                        Show
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href="#"
-                        class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                      >
-                        Edit
-                      </a>
-                    </li>
-                  </ul>
-                  <div class="py-1">
-                    <a
-                      href="#"
-                      class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                    >
-                      Delete
-                    </a>
-                  </div>
-                </div>
+          <tbody className="bg-white shadow-md dark:bg-gray-800 rounded-b">
+            <tr class="border-b dark:border-gray-700 ">
+              <td class="px-4 py-3">Fadhil P</td>
+              <td class="px-4 py-3">9876543210</td>
+              <td class="px-4 py-3">January 19</td>
+              <td class="px-4 py-3">180</td>
+              <td class="px-4 py-3">65</td>
+              <td class="px-4 py-3">Photo</td>
+              <td class="px-4 py-3">Done</td>
+              <td class="px-4 py-3 flex items-center justify-evenly">
+                <FontAwesomeIcon
+                  className="text-blue-500 cursor-pointer hover:text-blue-700"
+                  icon={faPenToSquare}
+                />
+                <FontAwesomeIcon
+                  className="text-red-500 cursor-pointer hover:text-red-700"
+                  icon={faTrash}
+                />
               </td>
             </tr>
           </tbody>
